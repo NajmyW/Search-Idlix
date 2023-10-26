96e0956bcd9eeda3a5c80208a4dc803ff2001d4d
NajmyW <120731308+NajmyW@users.noreply.github.com>
2023-10-26 09:39:54 +0700

    Create idlix.js

---
+++ idlix.js
@@ -0,0 +1,19 @@
+async function idlix(query) {
+  const { data } = await axios.get(
+    "https://tv2.idlixplus.net/wp-json/dooplay/search/?keyword=" +
+      query +
+      "&nonce=f83beb327e"
+  );
+  const res = Object.keys(data);
+  const result = res.map((i) => {
+    return {
+      title: data[i].title,
+      rating_imdb: data[i].extra.imdb,
+      release: data[i].extra.date,
+      url: data[i].url,
+      thumbnail: data[i].img,
+    };
+  });
+  return result;
+}
+idlix("Marvel");
